48bda9634d26db577d0617fc70b6810259748d5c
Manu8598 <emanuele.finocchiaro@nnamu.com>
2024-09-18 13:13:31 +0200

    Add files via upload

---
+++ nnamu-as-products-xapi-veYoF0sW/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://h8CIdNyX-KM0hbZJgUzAAHPR3ATYHMcR_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "nnamu as Products"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;&lt;span&gt;&lt;span style=&#34;color: rgb(0, 0, 0); text-decoration: inherit&#34;&gt;Welcome to the &lt;/span&gt;&lt;/span&gt;&lt;span style=&#34;color: rgb(0, 0, 0); text-decoration: inherit&#34;&gt;&lt;strong&gt;nnamu as Products Course&lt;/strong&gt;&lt;/span&gt;&lt;span&gt;&lt;span style=&#34;color: rgb(0, 0, 0); text-decoration: inherit&#34;&gt;, designed to introduce you to the innovative products we’re building. &lt;/span&gt;&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;/p&gt;&lt;p&gt;&lt;span&gt;&lt;span style=&#34;color: rgb(0, 0, 0); text-decoration: inherit&#34;&gt;This course will give you a clear understanding of our products, their purpose, and the challenges they&#39;re solving, helping you get up to speed quickly on how we’re driving success in procurement and beyond.&lt;/span&gt;&lt;/span&gt;&lt;/p&gt;"
+};
